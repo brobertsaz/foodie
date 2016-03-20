8bc0ba109f2e6c8ce25fc441a44db42920af966a
Bob Roberts <broberts_az@yahoo.com>
2016-03-19 22:18:37 -0700

    comment out validations for rake dbseed

--- app/models/food_truck.rb
+++ app/models/food_truck.rb
@@ -2,6 +2,6 @@ class FoodTruck < ActiveRecord::Base
   geocoded_by :address
 
   validates :address, presence: :true
-  validates :latitude, presence: :true
-  validates :longitude, presence: :true
+  # validates :latitude, presence: :true
+  # validates :longitude, presence: :true
 end
